a2983543ada947d77ffe22d70b3aaf577e4cd830
HackoTron <jw50075@gmail.com>
2017-04-07 20:18:41 +1200

    Added saving credential system

--- EpicLogin/EpicLogin/LoginSystem.cpp
+++ EpicLogin/EpicLogin/LoginSystem.cpp
@@ -11,12 +11,21 @@
 #include <windows.h>
 #include "NewAccount.h"
 #include <time.h>
+#include <fstream>
+
+using std::ifstream;
+using std::ofstream;
 
 int main();
 bool wantLogon;
 std::string username;
 std::string password;
 NewAccount account1;
+int option;
+ifstream credentialFileRead;
+ofstream credentialFileWrite;
+std::string user;
+std::string pass;
 
 NewAccount::NewAccount() {
 	username = "user";
@@ -52,19 +61,20 @@ void showOptions() {
 
 	int option;
 	std::cin >> option;
-
+	
 	switch (option) {
-	case 1: 
+	case 1:
+		credentialFileWrite.open("username.txt");
 		std::cout << "What would you like your username to be? > " << std::flush;
 		std::cin >> username;
+		credentialFileWrite << username;
+		credentialFileWrite.close();
 
+		credentialFileWrite.open("password.txt");
 		std::cout << "What would you like your password to be? > " << std::flush;
 		std::cin >> password;
-
-		if (password == " ") {
-			std::cout << "What would you like your password to be? > " << std::flush;
-			std::cin >> password;
-		}
+		credentialFileWrite << password;
+		credentialFileWrite.close();
 
 		account1.setCredentials(username, password);
 		wantLogon = true;
@@ -72,33 +82,33 @@ void showOptions() {
 		system("cls");
 		break;
 
-	case 2: 
+	case 2:
 		std::cout << "Login service initializing ..." << std::endl;
 		wantLogon = true;
 		Sleep(1000);
 		system("cls");
 		break;
-	
 
-	case 3: 
+
+	case 3:
 		std::cout << "Sorry, service not available\n" << std::endl;
 		Sleep(1000);
 		system("cls");
 		wantLogon = false;
 		break;
-	
-	case 4: 
+
+	case 4:
 		std::cout << "Quitting program ..." << std::endl;
 		Sleep(1000);
 		system("taskkill /f /im EpicLogin.exe >nul");
 		system("cls");
-	
 
-	default: 
+
+	default:
 		std::cout << '"' << option << '"' << " is not a valid option\n" << std::endl;
 		Sleep(1000);
 		system("cls");
-		showOptions();
+		main();
 	}
 
 	if (wantLogon == true) {
@@ -126,26 +136,26 @@ void showOptionsAuth(std::string name) {
 		std::cin >> option;
 
 		switch (option) {
-		case 1: 
+		case 1:
 			std::cout << "Sorry, service currently not available" << std::endl;
 			Sleep(1000);
 			system("cls");
 			break;
-		
-		case 2: 
+
+		case 2:
 			std::cout << "Logging out ... " << std::endl;
 			authenticated = false;
 			Sleep(1000);
 			system("cls");
 			break;
-		
-		case 3: 
+
+		case 3:
 			std::cout << "Quitting program ..." << std::endl;
 			Sleep(1000);
 			system("taskkill /f /im EpicLogin.exe >nul");
 			system("cls");
-		
-		default: 
+
+		default:
 			std::cout << '"' << option << '"' << " is not a valid option\n" << std::endl;
 			Sleep(1000);
 			system("cls");
@@ -170,7 +180,11 @@ void passwordSystem() {
 		std::cout << "Enter your password > " << std::flush;
 		std::cin >> inputPassword;
 
-		if (inputPassword == account1.getPassword()) {
+		credentialFileRead.open("password.txt");
+		credentialFileRead >> pass;
+
+		if (inputPassword == pass) {
+			credentialFileRead.close();
 			break;
 		}
 		else {
@@ -198,7 +212,11 @@ void usernameSystem() {
 		std::cout << "Enter your username > " << std::flush;
 		std::cin >> inputUsername;
 
-		if (inputUsername == account1.getUsername()) {
+		credentialFileRead.open("username.txt");
+		credentialFileRead >> user;
+
+		if (inputUsername == user) {
+			credentialFileRead.close();
 			break;
 		}
 		else {
